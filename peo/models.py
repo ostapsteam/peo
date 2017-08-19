028f991c24c762bb302df37662c1d11a5099c855
Alex Korotkov <durm.icecoffee@gmail.com>
2017-08-19 15:24:14 +0300

    inheritance

--- peo/models.py
+++ peo/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-from protos.models.proto import GeoProto, Proto
+from protos.models.proto import GeoProto
 
 
-class Lab(GeoProto, Proto):
-    pass
+class Lab(GeoProto):
+
+    def __unicode__(self):
+        return "Lab {}".format(
+            self.id
+        )
