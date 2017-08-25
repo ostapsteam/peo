055a092c46994607c93047834d7ead9c04fc48c1
Alex Korotkov <durm.icecoffee@gmail.com>
2017-08-25 22:56:10 +0300

    migrate

--- peo/manage.py
+++ peo/manage.py
@@ -8,32 +8,45 @@ BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 def main():
-    alembic = CommandLine()
-    alembic.parser.add_argument(
-        "--app-config",
-        dest="app_config",
-        required=True,
-        help="Config for app"
-    )
+    current_dir = os.path.abspath(os.getcwd())
 
-    options = alembic.parser.parse_args()
+    try:
+        alembic = CommandLine()
+        alembic.parser.add_argument(
+            "--app-config",
+            dest="app_config",
+            required=True,
+            help="Config for app"
+        )
 
-    app_config = get_config(options.app_config)
+        options = alembic.parser.parse_args()
 
-    if options.config == 'alembic.ini':
-        options.config = os.path.join(BASE_PATH, options.config)
+        os.chdir(
+            os.path.dirname(
+                os.path.abspath(
+                    options.config
+                )
+            )
+        )
 
-    cfg = Config(
-        file_=options.config,
-        ini_section=options.name,
-        cmd_opts=options
-    )
+        app_config = get_config(options.app_config)
 
-    cfg.set_main_option('script_location', str(os.path.join(BASE_PATH, 'alembic')))
-    cfg.set_main_option('sqlalchemy.url', str(app_config['database']))
+        if options.config == 'alembic.ini':
+            options.config = os.path.join(BASE_PATH, options.config)
 
-    if 'cmd' not in options:
-        alembic.parser.error("too few arguments")
-        exit(128)
+        cfg = Config(
+            file_=options.config,
+            ini_section=options.name,
+            cmd_opts=options
+        )
 
-    exit(alembic.run_cmd(cfg, options))
+        cfg.set_main_option('script_location', str(os.path.join(BASE_PATH, 'alembic')))
+        cfg.set_main_option('sqlalchemy.url', str(app_config['database']))
+
+        if 'cmd' not in options:
+            alembic.parser.error("too few arguments")
+            exit(128)
+
+        exit(alembic.run_cmd(cfg, options))
+    finally:
+        os.chdir(current_dir)
