ee59053630e1f5868ed42019d26e604b29515d71
Alex Korotkov <durm.icecoffee@gmail.com>
2017-08-26 08:59:05 +0300

    add alembic ini to setup manifest

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='peo',
-    version='0.19',
+    version='0.20',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
