cabb45d42eabe9e806c633c18390c2999054437e
Alex Korotkov <durm.icecoffee@gmail.com>
2017-08-26 11:49:39 +0300

    test lab models

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,36 @@
+import os
 from tempfile import mktemp
 from unittest import TestCase
 
+import logging
 from sqlalchemy import create_engine
+from alembic.config import Config
+from alembic.command import upgrade as upgrade_database
 
 from peo.db import DB
+import peo
 
 
 class DBTestCase(TestCase):
 
     def setUp(self):
-        return
-        self.DB = DB.configure(
-            create_engine(mktemp())
+        self.__db_file = "sqlite:///" + mktemp()
+        self.DB_URL = self.__db_file
+        DB.configure(
+            create_engine(self.DB_URL)
         )
+        self.db_session = DB.Session
+
+        MODULE_PATH = os.path.abspath(os.path.dirname(peo.__file__))
+        config = Config(os.path.join(MODULE_PATH, 'alembic.ini'), 'alembic')
+        #logging.getLogger('alembic.runtime.migration').disabled = True
+
+        config.set_main_option('script_location', str(os.path.join(MODULE_PATH, 'alembic')))
+        config.set_main_option('sqlalchemy.url', self.DB_URL)
+        upgrade_database(config, 'head')
+
+        super().setUp()
+
+    def tearDown(self):
+        if os._exists(self.__db_file):
+            os.remove(self.__db_file)
--- tests/models/test_labs.py
+++ tests/models/test_labs.py
@@ -1,6 +1,53 @@
+import uuid
+
 from peo.models.lab import Lab
-from unittest import TestCase
+from tests import DBTestCase
 
 
-class TestLabModels():
-    pass
+class TestLabModels(DBTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+    def test_lab_get(self):
+        session = self.db_session()
+
+        name = "Lab1"
+        desc = "Test desc"
+
+        lab1 = Lab(
+            name=name,
+            desc=desc
+        )
+        session.add(lab1)
+        session.flush()
+
+        self.assertIsNotNone(lab1.id)
+
+        lab2 = Lab.get_by_name(session, lab1.name)
+
+        self.assertEqual(lab1.id, lab2.id)
+
+        with self.assertRaises(Lab.DoesNotExist):
+            Lab.get(session, 0)
+
+    def test_get_by_name(self):
+        session = self.db_session()
+
+        name = "Lab1"
+        desc = "Test desc"
+
+        lab1 = Lab(
+            name=name,
+            desc=desc
+        )
+        session.add(lab1)
+        session.flush()
+
+        self.assertIsNotNone(lab1.id)
+
+        lab2 = Lab.get(session, lab1.id)
+
+        self.assertEqual(lab1.id, lab2.id)
+
+        self.assertIsNone(Lab.get_by_name(session, str(uuid.uuid4())))
