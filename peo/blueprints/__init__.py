9d78f8931f3781f0bddda447836557c45f5371c2
Korotkov Alex <durm.icecoffee@gmail.com>
2017-09-10 02:01:04 +0300

    schema result in req

--- peo/blueprints/__init__.py
+++ peo/blueprints/__init__.py
@@ -13,14 +13,14 @@ def get_error_resp(exc_dict):
 def process_request(f, *args, input_schema=None, output_schema=None, **kwargs):
     content = request.get_json(silent=False)
     if input_schema:
-        _, errors = input_schema.load(content)
+        req, errors = input_schema.load(content)
         if errors:
             return get_error_resp({
                 "message": "Bad request",
                 "status": 400,
                 "errors": errors
             })
-        resp = f(content, *args, **kwargs)
+        resp = f(req, *args, **kwargs)
     else:
         resp = f(*args, **kwargs)
     if isinstance(resp, Response):
